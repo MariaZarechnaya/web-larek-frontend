3af272a57aac3af2fa94bab762bf5461d986b472
Maria Zarechnaya <m.zarechnaya.m@mail.ru>
2024-05-21 19:22:01 +0300

    документация, описание типов, схема

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+
+import { IEvents } from "../components/base/events";
+
+interface ICard {
+    id: string,
+    category: string;
+    name: string;
+    text:string;
+    link:string;
+    price:number;
+}
+type Pay = 'Онлайн'| 'Наличные'
+
+interface IUser {
+    paymentType: Pay;
+    adress:string;
+    email:string;
+    phone:string;
+}
+
+interface ICardList  {
+    total:number;
+    cards: ICard[]  
+    preview: string | null 
+}
+interface IBacket {
+    items: Map <string, number>
+    sum:number
+}
+
+type basketCard = Pick <ICard,"id"|"name"|"price">  
+type previewCard = Pick <ICard, "category"|"name"| "link"|"price" >
+
+export interface IApi {
+    baseUrl: string;
+    get<T>(uri: string): Promise<T>;
+    post<T>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+}
+
+type ApiPostMethods = 'POST' | 'PUT' | 'DELETE' | 'PATCH';
